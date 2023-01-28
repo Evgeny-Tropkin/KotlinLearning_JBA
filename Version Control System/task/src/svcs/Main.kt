ca709dda470ea87f36ddda640a43148cc2e7b900
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2023-01-28 23:04:15 +0300

    Add basic help content data

--- Version Control System/task/src/svcs/Main.kt
+++ Version Control System/task/src/svcs/Main.kt
@@ -1,5 +1,11 @@
 package svcs
 
 fun main(args: Array<String>) {
-
+    val helpContent = mapOf(
+        "config" to "Get and set a username.",
+        "add" to "Add a file to the index.",
+        "log" to "Show commit logs.",
+        "commit" to "Save changes.",
+        "checkout" to "Restore a file."
+    )
 }
