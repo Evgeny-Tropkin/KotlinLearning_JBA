8f143531c10544a7132297a6635cf71dc7597613
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2023-01-29 08:02:10 +0300

    add printHelp function and read from standard input
    - printHelp function prints the whole help content
    - fun main() without args

--- Version Control System/task/src/svcs/Main.kt
+++ Version Control System/task/src/svcs/Main.kt
@@ -1,6 +1,6 @@
 package svcs
 
-fun main(args: Array<String>) {
+fun main() {
     val helpContent = mapOf(
         "config" to "Get and set a username.",
         "add" to "Add a file to the index.",
@@ -8,4 +8,12 @@ fun main(args: Array<String>) {
         "commit" to "Save changes.",
         "checkout" to "Restore a file."
     )
+    val helpArgumentValue = readln()
+    if (helpArgumentValue in helpContent) println(helpContent[helpArgumentValue])
+    else if (helpArgumentValue == "--help" || helpArgumentValue.isEmpty()) printHelp(helpContent)
+}
+
+fun printHelp(content: Map<String, String>) {
+    println("These are SVCS commands:")
+    for (key:String in content.keys) println(content[key])
 }
