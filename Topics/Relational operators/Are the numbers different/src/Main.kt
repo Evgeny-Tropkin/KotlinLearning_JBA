6b5e22b5f4295bd9b6528bd20fbf0ed1c13bd7b9
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2023-01-29 10:01:16 +0300

    "Topics/Relational operators/Are the numbers different" problem solved

--- Topics/Relational operators/Are the numbers different/src/Main.kt
+++ Topics/Relational operators/Are the numbers different/src/Main.kt
@@ -1,4 +1,14 @@
 fun main() {
-    // put your code here
+    val sizeOfInput = 3
+    val numbers = IntArray(sizeOfInput) { readln().toInt() }
+    var result = "true"
 
+    repeat(sizeOfInput){
+        val currentValue = numbers[it]
+        val subArr = numbers.slice(it + 1 until numbers.size)
+        for (item in subArr){
+            if (item == currentValue) result = "false"
+        }
+    }
+    println(result)
 }
