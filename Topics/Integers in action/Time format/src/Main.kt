f8629bf9adb6f7f22eff736c3e1a2357e7d723b2
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2023-02-04 14:53:29 +0300

    Topics\Integers in action: "Time format" problem solved

--- Topics/Integers in action/Time format/src/Main.kt
+++ Topics/Integers in action/Time format/src/Main.kt
@@ -1,4 +1,10 @@
 fun main() {
     val totalSeconds = System.currentTimeMillis() / 1000 // dont change this line
-    // enter your code
+    // solution
+    val secondsFromMidnight = totalSeconds % (24 * 3600)
+    val hours = secondsFromMidnight / 3600
+    val minutes = (secondsFromMidnight % 3600) / 60
+    val seconds = secondsFromMidnight % 60
+
+    println("$hours:$minutes:$seconds")
 }
