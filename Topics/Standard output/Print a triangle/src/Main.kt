936c5fe5862672ea41dbcdfd7f49915c8cbc2419
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2023-02-05 09:18:05 +0300

    Topics\Standard output: "Print a triangle" problem solved

--- Topics/Standard output/Print a triangle/src/Main.kt
+++ Topics/Standard output/Print a triangle/src/Main.kt
@@ -1,3 +1,8 @@
+const val RIGHT_TRIANGLE_HEIGHT = 6
+const val BLOCK = "#"
 fun main() {
-    // show me what you got...
+    for (rowNum in 1..RIGHT_TRIANGLE_HEIGHT) {
+        print(BLOCK.repeat(rowNum))
+        if (rowNum < RIGHT_TRIANGLE_HEIGHT) print('\n')
+    }
 }
