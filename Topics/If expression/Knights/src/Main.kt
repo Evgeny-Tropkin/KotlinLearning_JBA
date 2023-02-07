1a19a827c88a359a948832fafd2815d9271d9bb9
evgeny <evgeny-tropkin@yandex.ru>
2023-02-07 21:57:17 +0300

    Topics\If expression: problem "Knights" solved

--- Topics/If expression/Knights/src/Main.kt
+++ Topics/If expression/Knights/src/Main.kt
@@ -1,3 +1,15 @@
+import kotlin.math.abs
+
 fun main() {
-    // write your code here
+    val firstKnightCoords = readln().split(' ')
+    val secondKnightCoords = readln().split(' ')
+
+    val deltaX1 = abs(firstKnightCoords[0].toInt() - secondKnightCoords[0].toInt()) == 1
+    val deltaY1 = abs(firstKnightCoords[1].toInt() - secondKnightCoords[1].toInt()) == 2
+    val deltaX2 = abs(firstKnightCoords[0].toInt() - secondKnightCoords[0].toInt()) == 2
+    val deltaY2 = abs(firstKnightCoords[1].toInt() - secondKnightCoords[1].toInt()) == 1
+
+    if (deltaX1 && deltaY1 || deltaX2 && deltaY2) {
+        println("YES")
+    } else println("NO")
 }
