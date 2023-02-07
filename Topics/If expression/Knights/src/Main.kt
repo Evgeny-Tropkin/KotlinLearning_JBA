7d94db203efdab9202f2de005998d4f462752be6
evgeny <evgeny-tropkin@yandex.ru>
2023-02-07 22:05:49 +0300

    Topics\If expression: problem "Knights" solution improved

--- Topics/If expression/Knights/src/Main.kt
+++ Topics/If expression/Knights/src/Main.kt
@@ -1,15 +1,9 @@
 import kotlin.math.abs
 
 fun main() {
-    val firstKnightCoords = readln().split(' ')
-    val secondKnightCoords = readln().split(' ')
+    // the first knight coordinates
+    val (x1, y1) = readln().split(' ').map { it.toInt() }
+    val (x2, y2) = readln().split(' ').map { it.toInt() }
 
-    val deltaX1 = abs(firstKnightCoords[0].toInt() - secondKnightCoords[0].toInt()) == 1
-    val deltaY1 = abs(firstKnightCoords[1].toInt() - secondKnightCoords[1].toInt()) == 2
-    val deltaX2 = abs(firstKnightCoords[0].toInt() - secondKnightCoords[0].toInt()) == 2
-    val deltaY2 = abs(firstKnightCoords[1].toInt() - secondKnightCoords[1].toInt()) == 1
-
-    if (deltaX1 && deltaY1 || deltaX2 && deltaY2) {
-        println("YES")
-    } else println("NO")
+    println(if (abs((x1 - x2) * (y1 - y2)) == 2) "YES" else "NO")
 }
