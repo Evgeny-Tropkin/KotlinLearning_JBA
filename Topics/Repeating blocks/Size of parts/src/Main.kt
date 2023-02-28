f516e5c82390bab58b78b7b1ee531a202dfc26c9
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2023-02-28 21:57:42 +0300

    Topics\Repeating blocks: problem "Size of part" solved

--- Topics/Repeating blocks/Size of parts/src/Main.kt
+++ Topics/Repeating blocks/Size of parts/src/Main.kt
@@ -1,3 +1,7 @@
 fun main() {
-    // put your code here
+    val res = MutableList(3) { 0 }
+    repeat(readln().toInt()) {
+        res[(readln().toInt() + 3) % 3] += 1
+    }
+    print(res.joinToString(" "))
 }
