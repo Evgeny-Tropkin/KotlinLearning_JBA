fed7214b3ec51cf37c1c8dfe777d311315b3ba3c
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2023-03-04 18:54:36 +0300

    Topics\Repeating blocks: problem "Maximum element divisible by 4" solved

--- Topics/Repeating blocks/Maximum element divisible by 4/src/Main.kt
+++ Topics/Repeating blocks/Maximum element divisible by 4/src/Main.kt
@@ -1,3 +1,11 @@
 fun main() {
-
+    val n = readln().toInt()
+    var res = 0
+    repeat(n) {
+        val element = readln().toInt()
+        if (element % 4 == 0) {
+            if (element > res) res = element
+        }
+    }
+    print(res)
 }
