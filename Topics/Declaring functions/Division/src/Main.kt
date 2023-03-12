e4da304560d52a6aed7dedbc35f162256d385b34
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2023-03-12 07:54:02 +0300

    Topics\Declaring functions: problem "Division" solution improved

--- Topics/Declaring functions/Division/src/Main.kt
+++ Topics/Declaring functions/Division/src/Main.kt
@@ -1,10 +1,5 @@
 // write your code here
-fun divide(first: Long, second: Long): Double{
-    if (second != 0L) {
-        val res = first / second
-        return res
-    }
-}
+
 /* Do not change code below */
 fun main() {
     val a = readLine()!!.toLong()
