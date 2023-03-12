6d9ec2ad3ba3524fb931a7d1ad0f6f27341539bb
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2023-03-12 08:31:03 +0300

    Topics\For loop and ranges: problem "The roots of equation" solved

--- Topics/For loop and ranges/The roots of equation/src/Main.kt
+++ Topics/For loop and ranges/The roots of equation/src/Main.kt
@@ -1,3 +1,7 @@
 fun main() {
-    // put your code here
+    // parameters of the cubic equation
+    val (a, b, c, d) = MutableList(4) { readln().toInt() }
+    for (x in 0..1000) {
+        if (a * (x * x * x) + b * (x * x) + c * x + d == 0) println(x)
+    }
 }
