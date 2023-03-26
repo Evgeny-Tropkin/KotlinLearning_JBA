ff91258584f7e97c32f796f2c5ff8ef85536d68b
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2023-03-26 09:46:26 +0300

    Topics\Standard input with Java Scanner: the topic added with the theory and tasks
    The tasks are:
    - "Print each word in a new line": the task added
    - "Four numbers": the problem solved earlier (in the browser)
    - "Statements": the problem solved earlier (in the browser)
    - "The suitable input": the problem solved earlier (in the browser)
    - "Scanner": the problem solved earlier (in the browser)
    - "Which input suits": the problem solved earlier (in the browser)
    - "A function for reading data": the problem solved earlier (in the browser)

---
+++ Topics/Standard input with Java Scanner/Four numbers/src/Main.kt
@@ -0,0 +1,14 @@
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    // put your code here
+    val num1 = scanner.nextInt().toString()
+    val num2 = scanner.nextInt().toString()
+    val num3 = scanner.nextInt().toString()
+    val num4 = scanner.nextInt().toString()
+    println(num1)
+    println(num2)
+    println(num3)
+    println(num4)
+}
