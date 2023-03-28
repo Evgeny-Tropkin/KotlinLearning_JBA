98ee38925cfcf9dd3fa6f4c5c8a2b056f22f7268
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2023-03-28 20:44:45 +0300

    Topics\For loop and ranges\The maximum pairwise product: solution improved

--- Topics/For loop and ranges/The maximum pairwise product/src/Main.kt
+++ Topics/For loop and ranges/The maximum pairwise product/src/Main.kt
@@ -1,5 +1,5 @@
 fun main() {
     val numCount = readln().toInt()
     val numbers = MutableList(numCount) { readln().toInt() }.sortedDescending()
-    if (numCount == 1) { println(numbers[0]) } else println(numbers[0] * numbers[1])
+    println(if (numCount == 1) numbers[0] else numbers[0] * numbers[1])
 }
