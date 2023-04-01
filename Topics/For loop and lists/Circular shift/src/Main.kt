e62375f1ec2cbac8de8590f80a7925eebe52a443
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2023-04-01 08:29:33 +0300

    Topics\For loop and lists: the problem "Circular shift" solved

--- Topics/For loop and lists/Circular shift/src/Main.kt
+++ Topics/For loop and lists/Circular shift/src/Main.kt
@@ -1,3 +1,9 @@
 fun main() {
     // write your code here
+    val size = readln().toInt()
+    val inp = MutableList(size) { readln().toInt() }
+    print(inp.lastOrNull())
+    for (pos in 0 until size - 1) {
+        print(" " + inp[pos])
+    }
 }
