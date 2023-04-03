e367e87b7861476952fda79f0423c91f02180e25
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2023-04-03 20:52:27 +0300

    Topics\For loop and lists: the "Two numbers in a row" solution improved

--- Topics/For loop and lists/Two numbers in a row/src/Main.kt
+++ Topics/For loop and lists/Two numbers in a row/src/Main.kt
@@ -1,16 +1,7 @@
 fun main() {
     val size = readln().toInt()
-    val list = MutableList(size) { readln().toInt() }
-    val inp = readln()
-    var res = "YES"
-
-    for (pos in 1 until list.size) {
-        val variant1 = list[pos - 1].toString() + " " + list[pos].toString()
-        val variant2 = list[pos].toString() + " " + list[pos - 1].toString()
-        if (variant1 == inp || variant2 == inp) {
-            res = "NO"
-            break
-        }
-    }
-    println(res)
+    val list = MutableList(size) { readln() }.joinToString(" ")
+    val inp = readln().split(' ').map { it.toInt()}
+    println(if (inp.joinToString(" ") in list ||
+                inp.reversed().joinToString(" ") in list) "NO" else "YES")
 }
