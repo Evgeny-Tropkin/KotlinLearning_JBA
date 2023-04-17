ff267e8db752b332e1d03c8c8e48c68bd1ed17e3
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2023-04-17 20:36:58 +0300

    Topics\Characters: the "Sequence" problem solved

--- Topics/Characters/Sequence/src/Main.kt
+++ Topics/Characters/Sequence/src/Main.kt
@@ -1,3 +1,11 @@
 fun main() {
     // put your code here
+    val firstChar = readln().first()
+    val secondChar = readln().first()
+
+    if (secondChar.code - firstChar.code == 1) {
+        val thirdChar = readln().first()
+        print(thirdChar.code - secondChar.code == 1)
+    }
+    else println(false)
 }
