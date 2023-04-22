09e736bafe41f5c94548a65d737dccad548ca580
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2023-04-22 18:32:41 +0300

    Topics\Map: the "FizzBuzz" problem solved

--- Topics/Map/FizzBuzz/src/Main.kt
+++ Topics/Map/FizzBuzz/src/Main.kt
@@ -1,3 +1,8 @@
 fun iterator(map: Map<String, Int>) {
     // put your code here
+    for (item in map) {
+        if (item.value % 3 == 0) println("Fizz")
+        else if (item.value % 5 == 0) println("Buzz")
+        else println("FizzBuzz")
+    }
 }
