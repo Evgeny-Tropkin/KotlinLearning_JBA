d0f1e01519202a57512757dd5393d4b348ecd655
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2023-04-28 07:24:15 +0300

    Topics\Map: the "Application" problem solved

--- Topics/Map/Application/src/Main.kt
+++ Topics/Map/Application/src/Main.kt
@@ -1,3 +1,8 @@
 fun bill(priceList: Map<String, Int>, shoppingList: MutableList<String>): Int {
     // put your code here
+    var sum = 0
+    for (item in shoppingList) {
+        if (priceList.containsKey(item)) sum += priceList[item]!!
+    }
+    return sum
 }
