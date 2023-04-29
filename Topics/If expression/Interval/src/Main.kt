aaf61f8806f11ea5063dc9e09d12a12193bf15b9
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2023-04-29 15:10:08 +0300

    Topics\If expression: the "Interval" problem solution improved

--- Topics/If expression/Interval/src/Main.kt
+++ Topics/If expression/Interval/src/Main.kt
@@ -1,6 +1,6 @@
 fun main() {
     // write your code here
     val num = readln().toInt()
-    val res = if ((num > -15 && num <= 12) || (num > 14 && num < 17) || (num >= 19)) "True" else "False"
+    val res = if (num in -14 .. 12 || num in 15 until 17 || num >= 19) "True" else "False"
     print(res)
 }
