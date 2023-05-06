6a74151e580d6cb0a0e6fe1239637790c3231d32
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2023-05-06 13:48:00 +0300

    Topics\Named arguments: the "Perimeter" problem solved

--- Topics/Named arguments/Perimeter/src/Main.kt
+++ Topics/Named arguments/Perimeter/src/Main.kt
@@ -1,3 +1,13 @@
-fun perimeter(x1: Double, y1: Double, x2: Double, y2: Double, x3: Double, y3: Double, x4: Double, y4: Double): Double {
-    return 0.0
+fun perimeter(x1: Double, y1: Double, x2: Double, y2: Double, x3: Double, y3: Double, x4: Double = x1, y4: Double = y1): Double {
+    data class Point(val x: Double, val y: Double)
+
+    val points = listOf(Point(x1, y1), Point(x2, y2), Point(x3, y3), Point(x4, y4), Point(x1, y1))
+    var p = 0.0
+
+    for (n in 0 until points.size - 1) {
+        val dx = points[n].x - points[n + 1].x
+        val dy = points[n].y - points[n + 1].y
+        p += Math.hypot(dx, dy)
+    }
+    return p
 }
