00845c806ee8be67b1e795355be38912ff06b21e
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2023-05-22 21:03:16 +0300

    Topics\When expression: the "Shape" problem solved

--- Topics/When expression/Shape/src/Main.kt
+++ Topics/When expression/Shape/src/Main.kt
@@ -1,3 +1,13 @@
 fun main(args: Array<String>) {
     // write your code here
+    val shapeNumber = readln().toInt()
+    val shape: String = when (shapeNumber) {
+        1 -> "square"
+        2 -> "circle"
+        3 -> "triangle"
+        4 -> "rhombus"
+        else -> ""
+    }
+    if (shape != "") print("You have chosen a $shape")
+    else print("There is no such shape!")
 }
