6e4cdc4d0a38e038bf939c1509fc76bdefab16c6
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2023-05-25 14:57:49 +0300

    Stage 2/4 "Add & config": implement the "config" command

--- Version Control System/task/src/svcs/Main.kt
+++ Version Control System/task/src/svcs/Main.kt
@@ -10,14 +10,22 @@ fun main(args: Array<String>) {
         "commit" to "     Save changes.",
         "checkout" to "   Restore a file."
     )
-    val argumentValue = if (args.isNotEmpty()) args[0].split(" ") else listOf()
     val workingDirectory = System.getProperty ("user.dir") + File.separator + "Version Control System"
     val vcsFolder = checkFolder(workingDirectory, "vcs")
     val configFile = checkFile(vcsFolder, "config.txt")
     val indexFile = checkFile(vcsFolder, "index.txt")
 
-    if (argumentValue.isEmpty() || argumentValue[0] == "--help") printHelp(commands)
-    else println("'${argumentValue[0]}' is not a SVCS command.")
+    if (args.isEmpty() || args[0] == "--help") printHelp(commands)
+    else if (args[0] in commands) {
+        when (args[0]) {
+            "config" -> {
+                if (args.size > 1) {
+                    setUserName(configFile, args[1])
+                } else getUserName(configFile)
+            }
+        }
+    }
+    else println("'${args[0]}' is not a SVCS command.")
 }
 
 fun printHelp(content: Map<String, String>) {
@@ -40,4 +48,15 @@ fun checkFile(folder: File, fileName:String): File {
         file.createNewFile()
     }
     return file
+}
+
+fun getUserName(configFile: File) {
+    val content = configFile.readLines()
+    if (content.isEmpty()) {
+        println("Please, tell me who you are.")
+    } else println("The username is ${content[0]}.")
+}
+
+fun setUserName(configFile: File, userName: String){
+    configFile.writeText(userName)
 }
