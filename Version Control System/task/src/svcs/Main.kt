2b2537172c187def2c711c289a0f55adab401f70
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2023-05-25 09:25:56 +0300

    Stage 2/4 "Add & config": check for the "vcs" folder existence
    - if it isn't exists, make it

--- Version Control System/task/src/svcs/Main.kt
+++ Version Control System/task/src/svcs/Main.kt
@@ -1,5 +1,7 @@
 package svcs
 
+import java.io.File
+
 fun main(args: Array<String>) {
     val commands = mapOf(
         "config" to "     Get and set a username.",
@@ -9,12 +11,23 @@ fun main(args: Array<String>) {
         "checkout" to "   Restore a file."
     )
     val argumentValue = if (args.isNotEmpty()) args[0].split(" ") else listOf()
+    val workingDirectory = System.getProperty ("user.dir") + File.separator + "Version Control System"
+    val vcsFolder = checkFolder(workingDirectory, "vcs")
 
     if (argumentValue.isEmpty() || argumentValue[0] == "--help") printHelp(commands)
-    else println("'$argumentValue' is not a SVCS command.")
+    else println("'${argumentValue[0]}' is not a SVCS command.")
 }
 
 fun printHelp(content: Map<String, String>) {
     println("These are SVCS commands:")
     for (key:String in content.keys) println(key + content[key])
+}
+
+fun checkFolder(path: String, folderName: String): File {
+    val separator = File.separator
+    val folder = File(path + separator + folderName)
+    if (!folder.exists()) {
+        folder.mkdir()
+    }
+    return folder
 }
