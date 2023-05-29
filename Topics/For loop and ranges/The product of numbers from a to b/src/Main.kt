73ed31fea34ae6401e3b3579f125170a1cb0db9f
evgeny <evgeny-tropkin@yandex.ru>
2023-05-29 09:16:14 +0300

    Topics/For loop and ranges: the "The product of numbers from a to b" problem solved

--- Topics/For loop and ranges/The product of numbers from a to b/src/Main.kt
+++ Topics/For loop and ranges/The product of numbers from a to b/src/Main.kt
@@ -1,3 +1,10 @@
 fun main() {
     // put your code here
+    val a = readln().toInt()
+    val b = readln().toInt()
+    var res = 1L
+    for(num in a until b) {
+        res *= num
+    }
+    println(res)
 }
