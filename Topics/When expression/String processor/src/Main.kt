0017a23c363bc40ce86e54ec8a371eb0993ad40b
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2023-06-07 22:33:03 +0300

    Topics\When expression: the "String processor" problem solved

--- Topics/When expression/String processor/src/Main.kt
+++ Topics/When expression/String processor/src/Main.kt
@@ -1,3 +1,14 @@
 fun main() {
     // write your code here
+    val inp = MutableList(3) { readln() }
+
+    when (inp[1]) {
+        "equals" ->  println(inp[0] == inp[2])
+
+        "plus" -> println(inp[0] + inp[2])
+
+        "endsWith" -> println(inp[0].endsWith(inp[2]))
+
+        else -> println("Unknown operation")
+    }
 }
