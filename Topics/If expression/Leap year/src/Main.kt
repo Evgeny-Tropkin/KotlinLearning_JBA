089f74ab92bee7c7fb4b38aa43db341f8c054a9a
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2023-06-27 20:15:09 +0300

    Topics\If expression: the "Leap year" problem solved

--- Topics/If expression/Leap year/src/Main.kt
+++ Topics/If expression/Leap year/src/Main.kt
@@ -1,3 +1,9 @@
 fun main() {
     // write your code here
+    val year = readln().toInt()
+    var isLeap = year % 4 == 0
+    if (isLeap && year % 100 == 0) {
+        isLeap = year % 400 == 0
+    }
+    print(if (isLeap) "Leap" else "Regular")
 }
