115c642eaa1f08e976184292505294a1f37def18
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2023-06-27 07:51:46 +0300

    course aux updates

--- Topics/For loop and lists/Two numbers in a row/src/Main.kt
+++ Topics/For loop and lists/Two numbers in a row/src/Main.kt
@@ -4,9 +4,10 @@ fun main() {
     val (x, y) = readln().split(' ').map { it.toInt() }
     var result = "YES"
 
-    for (pos in 0 until  list.lastIndex) {
+    for (pos in 0 until list.lastIndex) {
         if ((list[pos] == x && list[pos + 1] == y) ||
-            (list[pos] == y && list[pos + 1] == x)) {
+            (list[pos] == y && list[pos + 1] == x)
+        ) {
             result = "NO"
             break
         }
--- Topics/For loop and ranges/Understanding ranges/src/Main.kt
+++ Topics/For loop and ranges/Understanding ranges/src/Main.kt
@@ -1,4 +1,4 @@
-// This is a matching task. You can use this editor as a playground
+// You can experiment here, it won’t be checked
 
 fun main(args: Array<String>) {
   // put your code here
