d785bfa539c359627d34998f24667662ba165381
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2023-07-12 20:40:45 +0300

    Topics\Named arguments: the "Car price" problem solved

--- Topics/Named arguments/Car price/src/Main.kt
+++ Topics/Named arguments/Car price/src/Main.kt
@@ -1,4 +1,7 @@
 // complete this function, add default values
-fun carPrice(old: Int, kilometers: Int, maximumSpeed: Int, automatic: Boolean){
-
+fun carPrice(old: Int = 5, kilometers: Int = 100000, maximumSpeed: Int = 120, automatic: Boolean = false){
+    val newPrice = 20000
+    val automat = if (automatic) 1500 else 0
+    val price = newPrice - old * 2000 + (maximumSpeed - 120) * 100 - (kilometers.div(10000)) * 200 + automat
+    print(price)
 }
